8ca51a50ce0b193815901f0b7b3896d1bc17be18
devanaclimgo <anaclimgo@gmail.com>
2025-02-17 20:15:04 -0300

    change list items dynamically

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,19 @@
 function ListGroup() {
+  const items = [
+    "New York",
+    "San Francisco",
+    "Amsterdan",
+    "London",
+    "Paris"
+  ]
+
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
-        <li className="list-group-item">Cras justo odio</li>
-        <li className="list-group-item">Dapibus ac facilisis in</li>
-        <li className="list-group-item">Morbi leo risus</li>
-        <li className="list-group-item">Porta ac consectetur ac</li>
-        <li className="list-group-item">Vestibulum at eros</li>
+        {items.map((item) => (
+          <li>{item}</li>
+        ))}
       </ul>
     </>
 )}
